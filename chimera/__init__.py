d82143fc6a1e078c1a248272f4bd9e2c5c5b1cae
Natasha Batalha <natasha.e.batalha@gmail.com>
2020-02-09 20:52:03 -0800

    package

---
+++ chimera/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from pkg_resources import declare_namespace
+    declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
+
+from .fm import *
